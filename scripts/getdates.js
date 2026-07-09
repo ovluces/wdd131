1c798ae897e762ac1703f8bdb5e2dd6e62604bcd
ovluces <ovluces@gmail.com>
2026-07-08 22:44:48 -0400

    Week02 update 1.1

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,3 @@
 document.getElementById("lastModified").innerHTML = document.lastModified;
 document.getElementById('currentyear').innerHTML = new Date().getFullYear();
+
--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,31 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('______'); // Fill in the blank to reference the unordered list element.
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+// const li = document.createElement('li');
+// const deleteButton = document.createElement('button');
+
+
+// wait for button clicks
+buttonElement.addEventListener("click", function () {
+  // Check if the user entered something
+  if (inputElement.value != "") {
+    // create list item and give it the value of the input
+    const li = document.createElement("li");
+    li.textContent = inputElement.value;
+    // create a button and add a click event listener
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "❌";
+    deleteBtn.addEventListener("click", function () {
+      listElement.removeChild(li);
+      inputElement.focus();
+    });
+    // add the button to the list item
+    li.appendChild(deleteBtn);
+    // OUTPUT: finally display the completed list item to the unordered list
+    listElement.appendChild(li);
+    // clear the user input field
+    inputElement.value = "";
+  }
+  // focus the user back to the input field
+  inputElement.focus();
+});
