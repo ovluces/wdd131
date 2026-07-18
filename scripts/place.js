91c1cb740bec0b5032063e00cda4009ea0a8d9ed
ovluces <ovluces@gmail.com>
2026-07-18 16:22:00 -0400

    place 1.7

--- scripts/place.js
+++ scripts/place.js
@@ -1,2 +1,19 @@
+let T_a = 10;
+let v = 5;
+
+function calculateWindChill(T_a, v) {
+  WindChill = 13.12 + 0.6215 * T_a - 11.37 * v ** 0.16 + 0.3965 * T_a * v ** 0.16;
+  WindChill = WindChill.toFixed(2)
+  return `${WindChill}`;
+}
+
+
 document.getElementById("lastModified").innerHTML = document.lastModified;
 document.getElementById('currentyear').innerHTML = new Date().getFullYear();
+
+if (T_a <= 10 && v > 4.8) {
+  document.getElementById('windchill').innerHTML = calculateWindChill(T_a, v);
+} else {
+  document.getElementById('windchill').innerHTML = "N/A";
+}
+
