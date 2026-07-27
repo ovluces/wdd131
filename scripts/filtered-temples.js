48b54ead4d0fde52aa3898bb507a3436cd2a5a8c
ovluces <ovluces@gmail.com>
2026-07-27 08:07:38 -0400

    ft3.0

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -13,8 +13,8 @@ document.getElementById('currentyear').innerHTML = new Date().getFullYear();
 
 const temples = [
   {
-    templeName: "Aba Nigeria",
-    location: "Aba, Nigeria",
+    templeName: "Aba Nigeria xxxxxxxxxx xxxxxxx",
+    location: "Aba, Nigeria, xxxx, xxxxxxx xxx xxx xx xxx xx xxx xx xx x xxx xxx xx xxx xxx xxx xx xx xx x",
     dedicated: "2005, August, 7",
     area: 11500,
     imageUrl:
